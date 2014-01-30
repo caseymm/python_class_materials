c4825c836beacebe86e7a16f49d98a43aa2ec36d
caseymm <caseymm@gmail.cpom>
2014-01-30 14:22:48 -0500

    renamed files and submitted find all version

--- caseymm_ex5.py
+++ caseymm_ex5.py
@@ -2,16 +2,6 @@ import re
 
 faculty = open("faculty.html","r")
 
-for aline in faculty:
-    
-    #values = aline.split()
-    match = re.search(r'[\w.-]+@[\w.-]+', aline)
-
-    if match:
-        print match.group()
-        #can only get ot print the first match on the page
-        
-    else:
-        print "no match"
-
-faculty.close()
+emails = re.findall(r'[\w\.-]+@[\w\.-]+', faculty.read())
+emails = list(set(emails))
+print emails
--- findall.py
+++
@@ -1,7 +0,0 @@
-import re
-
-faculty = open("faculty.html","r")
-
-emails = re.findall(r'[\w\.-]+@[\w\.-]+', faculty.read()) 
-for email in emails:
-    print email
