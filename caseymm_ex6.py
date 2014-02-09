fe4c9fbf9e7b120d0ea6ca906afabe7e78c267a7
caseymm <caseymm@gmail.com>
2014-02-09 13:23:46 -0500

    ex 6 failure

--- caseymm_ex6.py
+++ caseymm_ex6.py
@@ -1,5 +1,7 @@
 import string 
- 
+
+
+
 def process_file(filename): 
     hist = dict() 
     fp = open(filename) 
@@ -7,15 +9,35 @@ def process_file(filename):
         
         key, value = line.strip().split()
         hist[key] = value
+        print key
         #need set default or check something to see if this is in the dictionary
         #hist[row[0]] should be referring to a list -> and run .append on the list
-        t = []
-        for key, value in hist.items():
-            t.append((value, key))
         
-        return t 
+        
+        for key in hist.items():
+            empty = []
+            if key not in empty:
+                empty.append(key)
+            #else:
+            #    empty[key].append(value)
+            return empty
+            print empty
+          
+    return hist
+    
 
-hist = process_file("courses1.txt")
+hist = process_file("courses2.txt")
 print hist
+#empty
 
 
+#def count_up(hist):
+#    t = []
+#    for key in hist.items():
+#        t.append((value, key))
+#        for value in hist.items():
+#            if value not in t():
+#                t.append(value)
+#    return t
+#t = count_up(hist)
+#print t
