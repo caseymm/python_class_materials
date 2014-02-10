cf9851ef2e3b5d58d410a38718c09d8bb3cc4acc
caseymm <caseymm@gmail.com>
2014-02-09 22:23:45 -0500

    holy fuck that took a long time

--- caseymm_ex6.py
+++ caseymm_ex6.py
@@ -7,7 +7,7 @@ for line in fp:
     row = line.split()
     courses = row[0]
     instructors = row[1] 
-
+    
     if courses in hist:
         hist[courses].append(instructors)
     else:
@@ -15,3 +15,24 @@ for line in fp:
 
 print hist
 
+course_numbers = list(hist.keys())
+print course_numbers
+
+print hist.get('523')
+
+course_list = []
+for key in hist:
+    if 'Capra' in hist[key]:
+        course_list.append(key)
+print course_list
+
+instructor_names = list(hist.values())
+instructor_list = []
+ 
+for i in instructor_names:
+    for name in i:
+        if name not in instructor_list:
+            instructor_list.append(name)
+
+print instructor_list
+    
