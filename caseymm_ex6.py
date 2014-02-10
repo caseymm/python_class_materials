7fe4b298901ac4f7fedb75825a7d9e740a169fc6
caseymm <caseymm@gmail.com>
2014-02-09 21:12:20 -0500

    finished ex6

--- caseymm_ex6.py
+++ caseymm_ex6.py
@@ -1,43 +1,17 @@
 import string 
+fp = open("courses2.txt", "r")
+hist = dict()
 
+for line in fp:
+    line = line.strip()
+    row = line.split()
+    courses = row[0]
+    instructors = row[1] 
 
+    if courses in hist:
+        hist[courses].append(instructors)
+    else:
+        hist[courses] = [instructors]
 
-def process_file(filename): 
-    hist = dict() 
-    fp = open(filename) 
-    for line in fp:
-        
-        key, value = line.strip().split()
-        hist[key] = value
-        print key
-        #need set default or check something to see if this is in the dictionary
-        #hist[row[0]] should be referring to a list -> and run .append on the list
-        
-        
-        for key in hist.items():
-            empty = []
-            if key not in empty:
-                empty.append(key)
-            #else:
-            #    empty[key].append(value)
-            return empty
-            print empty
-          
-    return hist
-    
-
-hist = process_file("courses2.txt")
 print hist
-#empty
 
-
-#def count_up(hist):
-#    t = []
-#    for key in hist.items():
-#        t.append((value, key))
-#        for value in hist.items():
-#            if value not in t():
-#                t.append(value)
-#    return t
-#t = count_up(hist)
-#print t
