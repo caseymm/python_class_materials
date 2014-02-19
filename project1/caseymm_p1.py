bb709de7f82f95e6ef7d93f7520ec519f55a5031
caseymm <caseymm@gmail.com>
2014-02-19 14:35:04 -0500

    took out fist for quer_word in query_words -> need to do the regex parsing of query

--- project1/caseymm_p1.py
+++ project1/caseymm_p1.py
@@ -18,6 +18,8 @@ def process_file(filename):
     
     fin = open("stopwords.txt","r")
     stop_words = dict()
+    title_nums = {}
+    
     for line in fin:
         line = line.rstrip()
         stop_words.setdefault(line,{})
@@ -29,38 +31,42 @@ def process_file(filename):
             linenum += 1
             inv.setdefault(linenum,[]).append(line.rstrip())
             
-            
             match = re.search(r"((([A-Z]+\S*[A-Z]+))\s)+$", line)
             if match:
                 title = match.group()
                 title = title.rstrip()
                 #print title
-                line = line.replace('-',' ')
+                line = line.replace('-',' ')  
             for word in line.split():
                 word = word.strip(string.punctuation + string.whitespace)
                 word = word.lower()
                 hist.setdefault(word,{}).setdefault(title,[]).append(linenum)
+                #doesn't work
+                #for title in hist[word]:
+                    #title_nums.setdefault(title,[]).append(linenum)
             
         else:
             if line.strip() == "THE BROTHERS GRIMM FAIRY TALES":
                 content = True
                 block = "THE BROTHERS GRIMM FAIRY TALES"
+    print title_nums
     
     for word in stop_words:
         if word in hist:
             del hist[word]
     
-    for query_word in query_words:
-        if query_word not in hist:
-            print query_word
-            print "--"
-        else:
-            query_upper = ("**"+ query_word.upper() +"**")
-            for title in hist[query_word]:
-                print "  " + title
+    #for query_word in query_words:
+    for title in hist[query]:
+        print "  " + title
+        for query_word in query_words:
+            if query_word not in hist:
+                print query_word
+                print "--"
+            else:
                 print "    " + query_word
+                query_upper = ("**"+ query_word.upper() +"**")
                 this_line = hist[query_word][title] 
-                
+                        
                 for number in this_line:
                     if number in inv.keys():
                         myline = inv.get(number)
