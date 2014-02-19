af9685400a1b108bad7b4b95577b62b65145dfd0
caseymm <caseymm@gmail.com>
2014-02-19 15:24:18 -0500

    will print results for multiple words in query

--- project1/caseymm_p1.py
+++ project1/caseymm_p1.py
@@ -18,7 +18,6 @@ def process_file(filename):
     
     fin = open("stopwords.txt","r")
     stop_words = dict()
-    title_nums = {}
     
     for line in fin:
         line = line.rstrip()
@@ -29,53 +28,57 @@ def process_file(filename):
             block += line
             if line.strip() == "End of Project Gutenberg\'s Grimms\' Fairy Tales, by The Brothers Grimm": break
             linenum += 1
-            inv.setdefault(linenum,[]).append(line.rstrip())
-            
+            printed_line = line.lower()
+            inv.setdefault(linenum,[]).append(printed_line.rstrip())
             match = re.search(r"((([A-Z]+\S*[A-Z]+))\s)+$", line)
+            
             if match:
                 title = match.group()
                 title = title.rstrip()
-                #print title
-                line = line.replace('-',' ')  
+                line = line.replace('-',' ')
+                
             for word in line.split():
                 word = word.strip(string.punctuation + string.whitespace)
                 word = word.lower()
                 hist.setdefault(word,{}).setdefault(title,[]).append(linenum)
-                #doesn't work
-                #for title in hist[word]:
-                    #title_nums.setdefault(title,[]).append(linenum)
             
         else:
             if line.strip() == "THE BROTHERS GRIMM FAIRY TALES":
                 content = True
                 block = "THE BROTHERS GRIMM FAIRY TALES"
-    print title_nums
     
     for word in stop_words:
         if word in hist:
             del hist[word]
     
-    #for query_word in query_words:
-    for title in hist[query]:
-        print "  " + title
-        for query_word in query_words:
-            if query_word not in hist:
-                print query_word
-                print "--"
-            else:
-                print "    " + query_word
-                query_upper = ("**"+ query_word.upper() +"**")
-                this_line = hist[query_word][title] 
+    print query
+    for query_word in query_words: ##maybe for query in hist?
+        if query_word not in hist:
+            print "--"
+        else:
+            for title in hist[query_word]:
+                print "  " + title
+                for query_word in query_words:
+                    if query_word not in hist:
+                        print "    " + query_word
+                        print "    --"
+                    else:
+                        print "    " + query_word
+                        query_upper = ("**"+ query_word.upper() +"**")
+                        word_hist = hist[query_word]
+                        this_line = word_hist.get(title, "empty")
                         
-                for number in this_line:
-                    if number in inv.keys():
-                        myline = inv.get(number)
-                        for i in myline:
-                            entry = "      " + str(number) + "  " + i.replace(query, query_upper)
-                            #print i.replace(query, query_upper)
-                            print entry
+                        if this_line == "empty":
+                            print "    --"
+                        else:            
+                            for number in this_line:
+                                if number in inv.keys():
+                                    myline = inv.get(number)
+                                    for i in myline:
+                                        entry = "      " + str(number) + "  " + i.replace(query_word, query_upper)
+                                        #print i.replace(query, query_upper)
+                                        print entry
     return hist
             
 hist = process_file('grimms.txt')
-
 
