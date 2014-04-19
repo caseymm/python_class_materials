01b49e75d14c22b46a59fc2301d84050e04e7429
caseymm <caseymm@gmail.com>
2014-04-19 16:17:20 -0400

    got 4 minus format

--- pandas/project_3/caseymm_p3.py
+++ pandas/project_3/caseymm_p3.py
@@ -3,6 +3,8 @@ import pandas as pd
 from numpy.random import randn
 import numpy as np
 
+pd.options.display.float_format = '{0:.2f}'.format
+
 artists_df = pd.read_csv('artists.dat', sep='\t')
 
 user_artists_df = pd.read_csv('user_artists.dat', sep='\t', index_col=['userID', 'artistID'])
@@ -45,10 +47,10 @@ print result
 
 print "Question 1"
 print
-most_popular = pd.merge(new_artists, sorted_artist_weights, left_on='artistID', right_index=True).sort('weight', ascending=False)[:10]
+most_popular = pd.merge(new_artists, sorted_artist_weights, left_on='artistID', right_index=True).sort('weight', ascending=False)
 
 #need to delete index from this
-print most_popular
+print most_popular[:10]
 
 print
 print "Question 2"
@@ -59,5 +61,16 @@ print
 print "Question 3"
 print
 artist_to_user = user_artists_df_b.sort_index().count(level=0)
-artist_to_user_merge = pd.merge(new_artists, artist_to_user, left_on='artistID', right_index=True).sort('weight', ascending=False)[:10]
-print artist_to_user_merge
+artist_to_user_merge = pd.merge(new_artists, artist_to_user, left_on='artistID', right_index=True).sort('weight', ascending=False)
+print artist_to_user_merge[:10]
+
+print
+print "Question 4"
+print
+most_popular.columns = ['artistID', 'artist', 'total_plays']
+average_merge = pd.merge(artist_to_user_merge, most_popular)
+fifty_plus =  (average_merge[average_merge['weight']>=50])
+
+fifty_plus['avg'] = (fifty_plus['total_plays'])/(fifty_plus['weight'])
+selected = fifty_plus[['artist','artistID','avg']]
+print selected.sort('avg', ascending=False)[:10]
